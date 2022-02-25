593a34c7b422d1ea4e4482199dcd9378dbfc6904
Austin Miller <austin.miller1424@gmail.com>
2022-02-24 20:47:05 -0600

    test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const routes = require('./controller');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,11 +29,15 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
+// turn on routes
+app.use(routes);
+
+// turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
