e4b3e5b233426d7ae5bd64d54426be8255e66c15
Austin Miller <austin.miller1424@gmail.com>
2022-02-28 20:56:14 -0600

    file structure, start to handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 
-const PORT = process.env.PORT || 3001;
-// import the Sequelize constructor
+// const PORT = process.env.PORT || 3001;
+// // import the Sequelize constructor
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
--- controller/index.js
+++ controller/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./html-routes/home-routes');
-// const rentRoutes = require('../controller/api/rent-routes')
+const homeRoutes = require('./home-routes');
+const rentRoutes = require('../controller/api/rent-routes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-// router.use('rent', rentRoutes);
+router.use('rent', rentRoutes);
 
 
 module.exports = router;
