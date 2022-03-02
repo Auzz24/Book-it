036fb4b5e835ca3e819b50b9c2e9631996948fc2
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2022-03-01 20:12:59 -0600

    Update index.js

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,17 +1,11 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 
-const userRoutes = require('./user-routes');
-
-
-router.use('/user', userRoutes);
-=======
 const bookRoutes = require('./book-routes');
 const rentRoutes = require('./rent-routes');
 
 router.use('/books', bookRoutes);
 router.use('/rent', rentRoutes);
-router.use('/buy', buyRoutes);
->>>>>>> 81ab4c9ace450a0bf07eec26213e366bea15b121
+
+
 
 module.exports = router;
