0c85d90922f33926d3796c2a5c7e11968d5d4905
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2022-03-01 20:13:39 -0600

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,14 +1,5 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
-=======
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const rentRoutes = require('../controller/api/rent-routes')
@@ -16,8 +7,5 @@ const rentRoutes = require('../controller/api/rent-routes')
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('rent', rentRoutes);
-router.use('buy', buyRoutes);
-
->>>>>>> 81ab4c9ace450a0bf07eec26213e366bea15b121
 
 module.exports = router;
